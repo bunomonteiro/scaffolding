3e22b9b9eb80dc904d860f6f172a266faeb32e97
Bruno Monteiro <bunomonteiro@gmail.com>
2024-08-31 14:54:32 -0300

    Ajuste na tela de login

--- src/views/auth/LoginView.vue
+++ src/views/auth/LoginView.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { ref } from 'vue';
+import { reactive } from 'vue';
 import { useRoute, useRouter } from 'vue-router'
 import * as Joi from 'joi'
 
@@ -16,7 +16,7 @@ const router = useRouter()
  * - just stores
  * - has no knowledge of View and ModelView
  */
-const model = ref({
+const model = reactive({
   username: null,
   password: null,
 
@@ -38,18 +38,18 @@ const viewModel = {
     username: Joi.string().min(3).max(32).required().label('Email'),
     password: Joi.string().min(5).max(100).required().label('Senha'),
   }).required().label('Formulário').messages(joiMessages),
-  
+
   doLogin() {
     viewModel.clearValidations()
 
-    const validation = viewModel.formSchema.validate({ 
-      username: model.value.username,
-      password: model.value.password,
+    const validation = viewModel.formSchema.validate({
+      username: model.username,
+      password: model.password,
     })
 
     if (validation.error) {
-      model.value.errors.username = getError(validation, 'username')
-      model.value.errors.password = getError(validation, 'password')
+      model.errors.username = getError(validation, 'username')
+      model.errors.password = getError(validation, 'password')
       return;
     }
 
@@ -70,15 +70,15 @@ const viewModel = {
   },
 
   clearValidations() {
-    model.value.errors.username = null
-    model.value.errors.password = null
+    model.errors.username = null
+    model.errors.password = null
   }
 }
 </script>
 
 <template>
-  <div class="page-login h-full min-h-screen grid grid-cols-12 flex items-center overflow-hidden">
-    <div class="block pb-12 px-12 border dark:border-zinc-950 rounded-lg bg-white dark:bg-zinc-900 col-span-12 sm:col-start-2 sm:col-span-10 sm:col-start-4 sm:col-span-6 lg:col-start-5 lg:col-span-4 xl:col-start-6 xl:col-span-3">
+  <div class="page-login h-full min-h-screen flex justify-center items-center overflow-hidden p-6">
+    <div class="block pb-12 px-12 border dark:border-zinc-950 rounded-lg bg-white dark:bg-zinc-900 w-full max-w-md">
       <div class="flex min-h-auto flex-col justify-center px-6 py-12 lg:px-8">
         <div class="sm:mx-auto">
           <img class="mx-auto h-10 w-auto" src="/images/logo.png">
@@ -91,10 +91,12 @@ const viewModel = {
           <div>
             <label for="email" class="block text-sm font-medium leading-6">E-mail</label>
             <div class="mt-2">
-              <InputText id="email" v-model="model.username" type="email" autocomplete="email" required :invalid="model.errors.username?.invalid"
+              <InputText id="email" v-model="model.username" type="email" autocomplete="email" required
+                :invalid="model.errors.username?.invalid"
                 class="block w-full rounded-md border-0 py-1.5 px-2 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-primary-700 sm:text-sm sm:leading-6">
               </InputText>
-              <Message v-if="model.errors.username?.invalid" severity="error" class="mt-1 text-xs">{{ model.errors.username.message }}</Message>
+              <Message v-if="model.errors.username?.invalid" severity="error" class="mt-1 text-xs">{{
+                model.errors.username.message }}</Message>
             </div>
           </div>
 
@@ -102,14 +104,17 @@ const viewModel = {
             <div class="flex items-center justify-between">
               <label for="currentPassword" class="block text-sm font-medium leading-6">Senha</label>
               <div class="text-sm">
-                <a href="#" class="font-semibold text-primary-500 hover:text-primary-700" @click.prevent="viewModel.goToPasswordRecovery()">Esqueceu a senha?</a>
+                <a href="#" class="font-semibold text-primary-500 hover:text-primary-700"
+                  @click.prevent="viewModel.goToPasswordRecovery()">Esqueceu a senha?</a>
               </div>
             </div>
             <div class="mt-2">
-              <Password inputId="currentPassword" v-model="model.password" :feedback="false" toggleMask class="w-full" required :invalid="model.errors.password?.invalid"
+              <Password inputId="currentPassword" v-model="model.password" :feedback="false" toggleMask class="w-full"
+                required :invalid="model.errors.password?.invalid"
                 inputClass="block w-full rounded-md border-0 py-1.5 px-2 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-primary-400 focus:ring-2 focus:ring-inset focus:ring-primary-700 sm:text-sm sm:leading-6">
               </Password>
-              <Message v-if="model.errors.password?.invalid" severity="error" class="mt-1 text-xs">{{ model.errors.password.message }}</Message>
+              <Message v-if="model.errors.password?.invalid" severity="error" class="mt-1 text-xs">{{
+                model.errors.password.message }}</Message>
             </div>
           </div>
 
@@ -123,7 +128,8 @@ const viewModel = {
         </form>
         <p class="mt-10 text-center text-sm">
           Não tem conta?
-          <a href="#" class="font-semibold leading-6 text-primary-500 hover:text-primary-700" @click.prevent="viewModel.goToSignup()">Cadastre-se</a>
+          <a href="#" class="font-semibold leading-6 text-primary-500 hover:text-primary-700"
+            @click.prevent="viewModel.goToSignup()">Cadastre-se</a>
         </p>
       </main>
     </div>
